27001c7c9f079c7957721cca5d8a099b09ae2d31
Augusto Silva <ei11113@fe.up.pt>
2014-04-01 09:58:53 +0100

    start

---
+++ src/EarleyParser.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class EarleyParser {
+
+	public EarleyParser() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void addWords(ArrayList<String> words)
+	{
+	}
+	
+	public void addGrammar(HashMap<Integer,ArrayList<ArrayList<Integer>>> grammar)
+	{
+		
+	}
+
+}
