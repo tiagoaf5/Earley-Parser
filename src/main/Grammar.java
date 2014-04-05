2932cd25136745069bc0355febaf138aa3d014e7
Tiago Fernandes <ei11054@fe.up.pt>
2014-04-05 18:08:41 +0100

    ended grammar parsing; started grammar unit tests

--- src/main/Grammar.java
+++ src/main/Grammar.java
@@ -20,26 +20,24 @@ public class Grammar {
 	HashMap<String, ArrayList<ArrayList<String>>> grammar = new HashMap<String, ArrayList<ArrayList<String>>>();
 	LinkedHashSet<String> productions = new LinkedHashSet<String>();
 
+	String startProduction;
+
 	public static void main(String[] args) {
-		Grammar p = new Grammar(args[0]);
 		try {
-			p.read_file();
-			p.semanticAnalysis();
+			new Grammar(args[0]);
 		} catch (GrammarErrorException e) {
 			System.err.println(e.getMessage());
 		}
 	}
 
 
-	public Grammar(String path) {
+	public Grammar(String path) throws GrammarErrorException {
 		filePath = path;
+		readFile();
+		semanticAnalysis();
 	}
 
-
-
-
-
-	private void read_file() throws GrammarErrorException {
+	private void readFile() throws GrammarErrorException {
 
 		File f = new File(filePath);
 
@@ -49,65 +47,65 @@ public class Grammar {
 		try (BufferedReader br = new BufferedReader(new FileReader(f))) {
 			String line = br.readLine();
 
+			int cont = 0;
+
 			while (line != null) {
+
 				System.out.println("LINE - " + line);
 
-				if (line.matches("[A-Za-z0-9]+ ::= (.*)")) {
+				if (line.matches("[A-Za-z0-9]+ ::= (.*)")) { //match Rule: production ::= body
 
 					String head = line.substring(0,line.indexOf("::=") - 1);
 					String body = line.substring(line.indexOf("::=") + 3);
-					
+
+					if(cont == 0)
+						startProduction = head;
+
 					productions.add(head); //add head to productions list
 
 					if (grammar.containsKey(head)) {
 						ArrayList<ArrayList<String>> bodies = grammar.get(head);
-
-						String[] tmp2 = body.split(RE_SPLIT_PIPES);
-
-						for (String i : tmp2) {
-							ArrayList<String> tmp = splitBySpace(i, true);
-							
-							//add non-terminals to productions list
-							for(String j: tmp){
-								if(j.charAt(0) != '\"')
-									productions.add(j);
-							}
-							
-							bodies.add(tmp);	
-						}
+						parseBody(body, bodies);
 
 					} else {
 						ArrayList<ArrayList<String>> bodies = new ArrayList<ArrayList<String>>();
 						grammar.put(head, bodies);
-
-						String[] tmp2 = body.split(RE_SPLIT_PIPES);
-
-						for (String i : tmp2) {
-							ArrayList<String> tmp = splitBySpace(i, true);
-
-							//add non-terminals to productions list
-							for(String j: tmp){
-								if(j.charAt(0) != '\"')
-									productions.add(j);
-							}
-							
-							bodies.add(tmp);	
-						}
+						parseBody(body, bodies);
 					}
 				} 
 				else {
 					String abc ="Invalid grammar! Doesn't follow:\n Non-Terminal ::= body";
 					throw new GrammarErrorException(abc);
 				}
-					
+
 
 				line = br.readLine();
+				cont++;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Tokens - " + grammar);
+
+		System.out.println("\nGrammar - " + grammar);
 		System.out.println("Non-Terminals - " + productions);
+		System.out.println("StartProduction - " + startProduction);
+	}
+
+
+	private void parseBody(String body, ArrayList<ArrayList<String>> bodies) {
+		String[] tmp2 = body.split(RE_SPLIT_PIPES);
+
+		for (String i : tmp2) {
+			ArrayList<String> tmp = splitBySpace(i, true);
+
+			//add non-terminals to productions list
+			for(String j: tmp){
+				if(j.charAt(0) != '\"')
+					productions.add(j);
+			}
+
+			bodies.add(tmp);	
+		}
 	}
 
 	void semanticAnalysis() throws GrammarErrorException {
@@ -115,35 +113,15 @@ public class Grammar {
 			if(!grammar.containsKey(x))
 				throw new GrammarErrorException("Production \"" + x + "\" doesn't have a body");
 	}
-	
+
 	ArrayList<String> splitBySpace(String subjectString, boolean textInsideInverted) {
-
-		/*ArrayList<String> matchList = new ArrayList<String>();
-		Pattern regex = Pattern.compile(RE_SPLIT_SPACES);
-		Matcher regexMatcher = regex.matcher(subjectString);
-
-		while (regexMatcher.find()) {
-			if (regexMatcher.group(1) != null) {
-				// Add double-quoted string without the quotes 
-				if(textInsideInverted)
-					matchList.add(regexMatcher.group(1));
-			} else if (regexMatcher.group(2) != null ) {
-				// Add single-quoted string without the quotes
-				if(textInsideInverted)
-					matchList.add(regexMatcher.group(2));
-			} else {
-				// Add unquoted word
-				matchList.add(regexMatcher.group());
-			}
-
-		} */
 		//http://stackoverflow.com/questions/366202/regex-for-splitting-a-string-using-space-when-not-surrounded-by-single-or-double
-		
+
 		ArrayList<String> matchList = new ArrayList<String>();
-		
+
 		Pattern regex = Pattern.compile(RE_SPLIT_SPACES);
 		Matcher regexMatcher = regex.matcher(subjectString);
-		
+
 		while (regexMatcher.find()) {
 			matchList.add(regexMatcher.group().trim());
 		} 
@@ -152,5 +130,30 @@ public class Grammar {
 	}
 
 
+	/**
+	 * @return the grammar
+	 */
+	public HashMap<String, ArrayList<ArrayList<String>>> getGrammar() {
+		return grammar;
+	}
+
+
+	/**
+	 * @return the productions
+	 */
+	public LinkedHashSet<String> getProductions() {
+		return productions;
+	}
+
+
+	/**
+	 * @return the startProduction
+	 */
+	public String getStartProduction() {
+		return startProduction;
+	}
+
+	
+	
 
 }
--- src/test/GrammarTest.java
+++ src/test/GrammarTest.java
@@ -2,13 +2,45 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+
+import main.Grammar;
+import main.GrammarErrorException;
+
 import org.junit.Test;
 
 public class GrammarTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGrammar1() throws GrammarErrorException {
+		
+		Grammar a = new Grammar("./ficheiros_teste/grammar1.1.txt");
+		LinkedHashSet<String> productions = a.getProductions();
+		HashMap<String, ArrayList<ArrayList<String>>> grammar = a.getGrammar();
+		
+		//start production
+		assertEquals(a.getStartProduction(), "P");
+		
+		
+		//Productions
+		ArrayList<String> p = new ArrayList<String>() {
+			private static final long serialVersionUID = 1L;
+		{
+			add("P");
+			add("S");
+			add("M");
+			add("T");
+		}};
+		
+		for(String i: p)
+			assertTrue(productions.contains(i));
+		
+		/*HashMap<String, String> test = new HashMap<String, String>(){{
+			    	 put("a","b");
+			    }
+		};*/
 	}
 
 }
