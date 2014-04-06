59ae9b657df086426779c8919a6e4478c40ac387
Augusto Silva <ei11113@fe.up.pt>
2014-04-06 17:59:37 +0100

    earleyParser wannabe

--- src/main/EarleyParser.java
+++ src/main/EarleyParser.java
@@ -1,79 +1,102 @@
-/*package main;
+package main;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
-
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 public class EarleyParser {
 
 	class State{
-		int i;
-		int left;
-		int current;
-		ArrayList<Integer> right;
-
-		State(int left, int current, ArrayList<Integer> right, int i)
+		int i; // pos na frase
+		String left;
+		int current; // pos na regra da gramatica
+		ArrayList<String> right;
+		
+		State(String left, int current, ArrayList<String> right, int i)
 		{
-
+			this.i = i;
+			this.left = left;
+			this.right = right;
+			this.current = current;
 		}
 
 	}
-
-	private ArrayList<String> words;
-	private HashMap<Integer,ArrayList<ArrayList<Integer>>> grammar;
-	private ConcurrentLinkedQueue<State> queue;
-
-	public EarleyParser(ArrayList<Integer> words, HashMap<Integer,ArrayList<ArrayList<Integer>>> grammar) {
+	
+	private Sentence words;
+	private HashMap<String,ArrayList<ArrayList<String>>> grammar;
+	private ArrayList<ConcurrentLinkedQueue<State>> charts;
+	
+	public EarleyParser(Sentence words, HashMap<String,ArrayList<ArrayList<String>>> grammar) {
 		this.words = words;
 		this.grammar = grammar;
+		this.charts = new ArrayList<ConcurrentLinkedQueue<State>>(words.getSentence().size());
 	}
 
 	public boolean run()
 	{
-		ArrayList<Integer> first = new ArrayList<Integer>(1);
-		queue.add(new State(0,0,first,0)); // ???
-
-		for(int i = 0; i < words.size(); i++)
+		//INICIALIZACAO
+		ArrayList<String> right_root = new ArrayList<String>(1);
+		right_root.add("S");
+		charts.get(0).add(new State("_ROOT",0,right_root,0));
+		
+		for(int i = 0; i < words.getSentence().size(); i++)
 		{
-			//for(in)
+			Iterator<State> it = charts.get(i).iterator();
+			while(it.hasNext())
+			{
+				State s = it.next();
+				//if(words.getSentence().get(i).startsWith("\"") || (s.current==s.right.size() && !words.getSentence().get(i).startsWith("\"")))
+				if(s.current==s.right.size()) // end of rule
+				{
+					//complete
+					completer(s,i);
+				} else
+				{
+					if(s.right.get(s.current).startsWith("\""))
+					{
+						//scanner
+						scanner(s,i);
+					}
+					else
+					{
+						//predictor
+						predictor(s,i);
+					}
+				}
+			}
 		}
-
-
 		return false;
 	}
 
-}*/
-/*
-	function EARLEY-PARSE(words, grammar)
-    ENQUEUE((Î³ â†’ â€¢S, 0), chart[0])
-    for i â†� from 0 to LENGTH(words) do
-        for each state in chart[i] do
-            if INCOMPLETE?(state) then
-                if NEXT-CAT(state) is a nonterminal then
-                    PREDICTOR(state, i, grammar)         // non-terminal
-                else do
-                    SCANNER(state, i)                    // terminal
-            else do
-                COMPLETER(state, i)
-        end
-    end
-    return chart
+	private void predictor(State s, int j) {
+		String B = s.right.get(s.current);
+		ArrayList<ArrayList<String>> rules = grammar.get(B);
+		for(ArrayList<String> rule : rules)
+		{
+			charts.get(j).add(new State(B,0,rule,j));
+		}
+	}
 
-procedure PREDICTOR((A â†’ Î±â€¢B, i), j, grammar)
-    for each (B â†’ Î³) in GRAMMAR-RULES-FOR(B, grammar) do
-        ADD-TO-SET((B â†’ â€¢Î³, j), chart[ j])
-    end
+	private void scanner(State s, int j) { // ?????
+		String B = s.right.get(s.current);
+		if(B.equals(words.getSentence().get(j)))
+		{
+			charts.get(j+1).add(new State(s.left,s.current+1,s.right,s.i));
+		}
+	}
 
-procedure SCANNER((A â†’ Î±â€¢B, i), j)
-    if B âŠ‚ PARTS-OF-SPEECH(word[j]) then
-        ADD-TO-SET((B â†’ word[j], i), chart[j + 1])
-    end
+	private void completer(State s, int k) {
+		Iterator<State> it = charts.get(s.i).iterator();
+		while(it.hasNext())
+		{
+			State currentState = it.next();
+			if(s.left.equals(currentState.right.get(currentState.current)))
+			{
+				State newState = new State(currentState.left,currentState.current+1,currentState.right,currentState.i);
+				charts.get(k).add(newState);
+			}
+		}
+	}
 
-procedure COMPLETER((B â†’ Î³â€¢, j), k)
-    for each (A â†’ Î±â€¢BÎ², i) in chart[j] do
-        ADD-TO-SET((A â†’ Î±Bâ€¢Î², i), chart[k])
-    end*/
-
-
+}
