06c68c2cbea1dbe2b1de09f7c4d43372e69430a3
dvn123 <diogovaznunes@gmail.com>
2014-05-31 00:13:32 +0100

    ,

--- src/main/EarleyParser.java
+++ src/main/EarleyParser.java
@@ -1,8 +1,22 @@
 package main;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.awt.*;
+import java.awt.geom.*;
+import main.*;
 
+
+import javax.swing.*;
+
+import org.jgraph.*;
+import org.jgraph.graph.*;
+import org.jgrapht.*;
+import org.jgrapht.ext.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.graph.DefaultEdge;
 
 class State{
 	int i; // pos na frase
@@ -10,29 +24,54 @@ class State{
 	int current; // pos na regra da gramatica
 	ArrayList<String> right;
 	ArrayList<State> parents; 
+	ListenableGraph<String, DefaultEdge> g;
+	JGraphModelAdapter<String, DefaultEdge> jgAdapter;
+	String previous;
 
-	State(String left, int current, ArrayList<String> right, int i)
-	{
+	State(String left, int current, ArrayList<String> right, int i, ListenableGraph<String, DefaultEdge> g, JGraphModelAdapter<String, DefaultEdge> jgAdapter)	{
 		this.i = i;
 		this.left = left;
 		this.right = right;
 		this.current = current;
+		this.g = g;
+		this.jgAdapter = jgAdapter;
 		parents = new ArrayList<State>();
 	}
 
-	public void parents(int level) //Print and visit parents
-	{
+	public void parents(int level, String previous) {//Print and visit parents
 		StringBuilder ss = new StringBuilder();
 		for(String word : right)
 			ss.append(word);
 		System.out.println(ss.toString());
-		for(State sparent : parents)
-		{
+		//boolean bela = false;
+		//if(!g.addVertex(ss.toString())) {
+			//bela = true;
+			g.addVertex(ss.toString());
+		//}
+		positionVertexAt(ss.toString(), (int) Math.floor(Math.random()%600), 75 * level);
+		if(previous != null)
+			g.addEdge(previous, ss.toString());
+		for(State sparent : parents) {
 			for(int i = 0; i <= level; i++)
 				System.out.print("   ");
-			sparent.parents(++level);
+			//if(bela)
+			//	sparent.parents(++level, ss.toString() + " ");
+			//else
+				sparent.parents(++level, ss.toString());
 		}
 	}
+	
+	@SuppressWarnings("unchecked") // FIXME hb 28-nov-05: See FIXME below
+    private void positionVertexAt(Object vertex, int x, int y) {
+        DefaultGraphCell cell = jgAdapter.getVertexCell(vertex);
+        AttributeMap attr = cell.getAttributes();
+        Rectangle2D bounds = GraphConstants.getBounds(attr);
+        Rectangle2D newBounds = new Rectangle2D.Double(x, y, bounds.getWidth(), bounds.getHeight());
+        GraphConstants.setBounds(attr, newBounds);
+        AttributeMap cellAttr = new AttributeMap();
+        cellAttr.put(cell, attr);
+        jgAdapter.edit(cellAttr, null, null, null);
+    }
 
 	public String toString()
 	{
@@ -71,36 +110,45 @@ class State{
 
 }
 
-public class EarleyParser {
-
-
+public class EarleyParser extends JApplet {
 	private Sentence words;
 	private HashMap<String,ArrayList<ArrayList<String>>> grammar;
 	private String start;
 	private ArrayList<ArrayList<State>> charts;
+
+	private static final long serialVersionUID = 3256444702936019250L;
+    private static final Color DEFAULT_BG_COLOR = Color.decode("#FAFBFF");
+    private static final Dimension DEFAULT_SIZE = new Dimension(800, 600);
+    private JGraphModelAdapter<String, DefaultEdge> jgAdapter;
+    private ListenableGraph<String, DefaultEdge> g;
 
 	public EarleyParser(Sentence words, Grammar grammar) {
 		this.words = words;
 		this.grammar = grammar.getGrammar();
 		this.start = grammar.getStartProduction();
 		this.charts = new ArrayList<ArrayList<State>>(words.getSentence().size()+1);
-		for(int i = 0; i < words.getSentence().size()+1; i++)
-		{
+		for(int i = 0; i < words.getSentence().size()+1; i++) {
 			this.charts.add(new ArrayList<State>());
 		}
 	}
 
-	public boolean run()
-	{
-
+	public boolean run() {
 		//INICIALIZACAO
 		ArrayList<String> right_root = new ArrayList<String>(1);
 		right_root.add(start);
-		State begin = new State("_ROOT",0,right_root,0);
+		State begin = new State("_ROOT",0,right_root,0, g, jgAdapter);
 		addIfNotContains(0,begin);
+	
+		g = new ListenableDirectedMultigraph<String, DefaultEdge>(DefaultEdge.class);
+        jgAdapter = new JGraphModelAdapter<String, DefaultEdge>(g);
 
-		for(int i = 0; i < words.getSentence().size()+1; i++)
-		{
+        JGraph jgraph = new JGraph(jgAdapter);
+
+        adjustDisplaySettings(jgraph);
+        getContentPane().add(jgraph);
+        resize(DEFAULT_SIZE);
+
+		for(int i = 0; i < words.getSentence().size()+1; i++) {
 			System.out.println("\nWord no "+i);
 			if(i < words.getSentence().size())
 				System.out.println(words.getSentence().get(i));
@@ -136,12 +184,12 @@ public class EarleyParser {
 		}
 
 		//TREE
-		State last_state = new State("_ROOT",1,right_root,0);
+		State last_state = new State("_ROOT",1,right_root,0, g, jgAdapter);
 		ArrayList<State> array = charts.get(charts.size()-1);
 		for(State s_root : array) 
 		{
 			if(s_root.equals(last_state))
-				s_root.parents(0);
+				s_root.parents(0, null);
 		}
 
 		boolean r = charts.get(charts.size()-1).contains(last_state);
@@ -154,10 +202,32 @@ public class EarleyParser {
 		for(ArrayList<String> rule : rules)
 		{
 			System.out.print("Predictor Action");
-			State snew = new State(B,0,rule,j);
+			State snew = new State(B,0,rule,j, g, jgAdapter);
 			addIfNotContains(j,snew);
 		}
 	}
+	
+	public static void main(String [] args) {
+        //JGraphAdapterDemo applet = new JGraphAdapterDemo();
+        //applet.init();
+        
+        Grammar g2;
+		try {
+			g2 = new Grammar("./ficheiros_teste/grammar1.txt");
+			Lines lines=new Lines("./ficheiros_teste/sentences1.txt");
+			EarleyParser ep = new EarleyParser(lines.getLines().get(0), g2);
+			boolean result = ep.run();
+			
+			JFrame frame = new JFrame();
+	        frame.getContentPane().add(ep);
+	        frame.setTitle("JGraphT Adapter to JGraph Demo");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.pack();
+	        frame.setVisible(true);
+		} catch (GrammarErrorException e) {
+			e.printStackTrace();
+		}
+    }
 
 	private void scanner(State s, int j) {		
 		String B = s.right.get(s.current);
@@ -172,7 +242,7 @@ public class EarleyParser {
 		if(epsilon)
 		{
 			System.out.print("Scanner Action epsilon");
-			State snew = new State(s.left,s.current+1,s.right,s.i);
+			State snew = new State(s.left,s.current+1,s.right,s.i, g, jgAdapter);
 			State newAdded = addIfNotContains(j,snew); //adds to current chart
 			for(State parent : s.parents){//copy parents from duplicated state
 				if(!newAdded.parents.contains(parent))
@@ -181,7 +251,7 @@ public class EarleyParser {
 		} else if(B.equals(words.getSentence().get(j))) 
 		{
 			System.out.print("Scanner Action");
-			State snew = new State(s.left,s.current+1,s.right,s.i);
+			State snew = new State(s.left,s.current+1,s.right,s.i, g, jgAdapter);
 			State newAdded = addIfNotContains(j+1,snew); //adds to next charts
 			for(State parent : s.parents){//copy parents from duplicated state
 				if(!newAdded.parents.contains(parent))
@@ -199,7 +269,7 @@ public class EarleyParser {
 			if(s.left.equals(currentState.right.get(currentState.current)))
 			{
 				System.out.print("Completer Action");
-				State newState = new State(currentState.left,currentState.current+1,currentState.right,currentState.i);
+				State newState = new State(currentState.left,currentState.current+1,currentState.right,currentState.i, g, jgAdapter);
 				State newAdded = addIfNotContains(k,newState);
 				newAdded.parents.add(s);
 				for(State parent : currentState.parents){ //copy parents from duplicated state
@@ -225,5 +295,29 @@ public class EarleyParser {
 		list.add(s);
 		return s;
 	}
+	
+	private void adjustDisplaySettings(JGraph jg) {
+        jg.setPreferredSize(DEFAULT_SIZE);
+        Color c = DEFAULT_BG_COLOR;
+        String colorStr = null;
+        try {
+            colorStr = getParameter("bgcolor");
+        } catch (Exception e) {
+        }
+        if (colorStr != null) {
+            c = Color.decode(colorStr);
+        }
+        jg.setBackground(c);
+    }
 
+    
+    /**
+     * a listenable directed multigraph that allows loops and parallel edges.
+     */
+    private static class ListenableDirectedMultigraph<V, E> extends DefaultListenableGraph<V, E> implements DirectedGraph<V, E> {
+        private static final long serialVersionUID = 1L;
+        ListenableDirectedMultigraph(Class<E> edgeClass) {
+            super(new DirectedMultigraph<V, E>(edgeClass));
+        }
+    }
 }
