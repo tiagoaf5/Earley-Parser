052423f62eed0e7d356bda11abfc9c9cac154686
Augusto Silva <ei11113@fe.up.pt>
2014-06-02 13:27:32 +0100

    getTrees option

--- src/main/EarleyParser.java
+++ src/main/EarleyParser.java
@@ -3,81 +3,106 @@ package main;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
-class State{
-	int i; // pos na frase
-	String left;
-	int current; // pos na regra da gramatica
-	ArrayList<String> right;
-	ArrayList<State> parents; 
-
-	State(String left, int current, ArrayList<String> right, int i)
-	{
-		this.i = i;
-		this.left = left;
-		this.right = right;
-		this.current = current;
-		parents = new ArrayList<State>();
-	}
-
-	public void parents(int level) //Print and visit parents
-	{
-		StringBuilder ss = new StringBuilder();
-		for(String word : right)
-			ss.append(word);
-		System.out.println(ss.toString());
-		for(State sparent : parents)
-		{
-			for(int i = 0; i <= level; i++)
-				System.out.print("   ");
-			sparent.parents(++level);
-		}
-	}
-
-	public String toString()
-	{
-		String out = left + "->";
-		for(int k = 0; k < right.size(); k++)
-		{
-			if(k==current)
-				out += "@";
-			out += right.get(k);	
-		}
-		if(right.size()==current)
-			out += "@";
-
-		return "("+out+","+i+")";
-	}
-
-	public boolean equals(Object obj) {
-		if(obj instanceof State)
-		{
-			State s2 = (State)obj;
-			if(i != s2.i)
-				return false;
-			if(current != s2.current)
-				return false;
-			if(!left.equals(s2.left))
-				return false;
-			if(right.size()!=s2.right.size())
-				return false;
-			for(int k = 0; k < right.size(); k++)
-				if(!right.get(k).equals(s2.right.get(k)))
-					return false;
-			return true;
-		}
-		return false;
-	}
-
-}
+import main.EarleyParser.State.Mypair;
 
 public class EarleyParser {
+	
+	class Node{
+		String text;
+		ArrayList<Node> siblings = new ArrayList<Node>();
+		
+		Node(String s)
+		{
+			text =s;
+		}
+	}
 
+	class State{
+		class Mypair{ //need this to keep the order
+			String key;
+			ArrayList<State> values;
+			Mypair(String key, ArrayList<State> values)
+			{
+				this.key = key;
+				this.values = values;
+			}
+		}
+		
+		int i; // pos na frase
+		String left;
+		int current; // pos na regra da gramatica
+		ArrayList<String> right;
+		ArrayList<Mypair> parents; // para cada right, tem varios estados pais 
+
+		State(String left, int current, ArrayList<String> right, int i)
+		{
+			this.i = i;
+			this.left = left;
+			this.right = right;
+			this.current = current;
+			parents = new ArrayList<Mypair>();
+			for(String r : right)
+			{
+				parents.add(new Mypair(r,new ArrayList<State>()));
+			}
+		}
+
+		public void parents(Node node_parent) //visit parents
+		{
+			for(Mypair pair : parents)
+			{
+				Node son = new Node(pair.key);
+				node_parent.siblings.add(son);
+				for(State sparent : pair.values)
+				{
+					sparent.parents(son);
+				}
+			}
+			
+		}
+
+		public String toString()
+		{
+			String out = left + "->";
+			for(int k = 0; k < right.size(); k++)
+			{
+				if(k==current)
+					out += "@";
+				out += right.get(k);	
+			}
+			if(right.size()==current)
+				out += "@";
+
+			return "("+out+","+i+")";
+		}
+
+		public boolean equals(Object obj) {
+			if(obj instanceof State)
+			{
+				State s2 = (State)obj;
+				if(i != s2.i)
+					return false;
+				if(current != s2.current)
+					return false;
+				if(!left.equals(s2.left))
+					return false;
+				if(right.size()!=s2.right.size())
+					return false;
+				for(int k = 0; k < right.size(); k++)
+					if(!right.get(k).equals(s2.right.get(k)))
+						return false;
+				return true;
+			}
+			return false;
+		}
+
+	}
 
 	private Sentence words;
 	private HashMap<String,ArrayList<ArrayList<String>>> grammar;
 	private String start;
 	private ArrayList<ArrayList<State>> charts;
+	private ArrayList<Node> trees;
 
 	public EarleyParser(Sentence words, Grammar grammar) {
 		this.words = words;
@@ -88,6 +113,11 @@ public class EarleyParser {
 		{
 			this.charts.add(new ArrayList<State>());
 		}
+	}
+	
+	public ArrayList<Node> getTrees()
+	{
+		return trees;
 	}
 
 	public boolean run()
@@ -138,10 +168,15 @@ public class EarleyParser {
 		//TREE
 		State last_state = new State("_ROOT",1,right_root,0);
 		ArrayList<State> array = charts.get(charts.size()-1);
+		trees = new ArrayList<Node>();
 		for(State s_root : array) 
 		{
 			if(s_root.equals(last_state))
-				s_root.parents(0);
+			{
+				Node root = new Node("_ROOT");
+				trees.add(root);
+				s_root.parents(root);
+			}
 		}
 
 		boolean r = charts.get(charts.size()-1).contains(last_state);
@@ -174,19 +209,37 @@ public class EarleyParser {
 			System.out.print("Scanner Action epsilon");
 			State snew = new State(s.left,s.current+1,s.right,s.i);
 			State newAdded = addIfNotContains(j,snew); //adds to current chart
-			for(State parent : s.parents){//copy parents from duplicated state
-				if(!newAdded.parents.contains(parent))
-					newAdded.parents.add(parent);
+			
+			for(Mypair right : s.parents){//copy parents from duplicated state
+					for(State parent : right.values)
+					{
+						for(Mypair newAddedRight : newAdded.parents)
+						{
+							if(!newAddedRight.values.contains(parent))
+							{
+								newAddedRight.values.add(parent);
+							}
+						}
+					}
 			}
 		} else if(B.equals(words.getSentence().get(j))) 
 		{
 			System.out.print("Scanner Action");
 			State snew = new State(s.left,s.current+1,s.right,s.i);
 			State newAdded = addIfNotContains(j+1,snew); //adds to next charts
-			for(State parent : s.parents){//copy parents from duplicated state
-				if(!newAdded.parents.contains(parent))
-					newAdded.parents.add(parent);
-			}
+			
+			for(Mypair right : s.parents){//copy parents from duplicated state
+				for(State parent : right.values)
+				{
+					for(Mypair newAddedRight : newAdded.parents)
+					{
+						if(!newAddedRight.values.contains(parent))
+						{
+							newAddedRight.values.add(parent);
+						}
+					}
+				}
+		}
 		}
 	}
 
@@ -201,14 +254,30 @@ public class EarleyParser {
 				System.out.print("Completer Action");
 				State newState = new State(currentState.left,currentState.current+1,currentState.right,currentState.i);
 				State newAdded = addIfNotContains(k,newState);
-				newAdded.parents.add(s);
-				for(State parent : currentState.parents){ //copy parents from duplicated state
-					if(!newAdded.parents.contains(parent))
-						newAdded.parents.add(parent);
-				}
+				//newAdded.parents.add(s);
+				for(Mypair pair : newAdded.parents)
+					if(pair.key.equals(s.left))
+					{
+						pair.values.add(s);
+						break;
+					}
+				
+				for(Mypair right : currentState.parents){//copy parents from duplicated state
+					for(State parent : right.values)
+					{
+						for(Mypair newAddedRight : newAdded.parents)
+						{
+							if(!newAddedRight.values.contains(parent))
+							{
+								newAddedRight.values.add(parent);
+							}
+						}
+					}
+			}
 			}
 		}
 	}
+
 
 	private State addIfNotContains(int num, State s)
 	{
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,39 @@
 package main;
 
+import java.util.ArrayList;
+
+
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args)
+	{
+		Grammar g;
+		try {
+			g = new Grammar("./ficheiros_teste/grammar1.txt");
+			Lines lines=new Lines("./ficheiros_teste/sentences1.txt");
+			EarleyParser ep = new EarleyParser(lines.getLines().get(0), g);
+			boolean result = ep.run();
+			System.out.println("Result: " + result);
+			ArrayList<EarleyParser.Node> roots =  ep.getTrees();
+			for(EarleyParser.Node root : roots)
+			{
+				showTree(root,0); //o level e so para os espacos, nao e necessario
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR");
+		}
 	}
-
+	
+	private static void showTree(EarleyParser.Node root,int level) //modificar para em vez de imprimir adicionar ao grafo
+	{
+		System.out.print(level);
+		for(int i = 0; i <= level; i++)
+			System.out.print(" ");
+		System.out.println(root.text);
+		for(EarleyParser.Node sibling : root.siblings)
+		{
+			showTree(sibling,level+1);
+		}
+	}
+	
 }
