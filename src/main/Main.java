750d8145fb524f88bbb34bded058ac9030707c42
Augusto Silva <ei11113@fe.up.pt>
2014-06-02 13:36:00 +0100

    MyTree

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,29 +2,18 @@ package main;
 
 import java.util.ArrayList;
 
-
-public class Main {
-
-	public static void main(String[] args)
-	{
-		Grammar g;
-		try {
-			g = new Grammar("./ficheiros_teste/grammar1.txt");
-			Lines lines=new Lines("./ficheiros_teste/sentences1.txt");
-			EarleyParser ep = new EarleyParser(lines.getLines().get(0), g);
-			boolean result = ep.run();
-			System.out.println("Result: " + result);
-			ArrayList<EarleyParser.Node> roots =  ep.getTrees();
-			for(EarleyParser.Node root : roots)
-			{
-				showTree(root,0); //o level e so para os espacos, nao e necessario
-			}
-		} catch (Exception e) {
-			System.out.println("ERROR");
-		}
+class MyTree
+{
+	EarleyParser.Node root;
+	//adicionar aqui objectos do grafo
+	public MyTree(EarleyParser.Node n) {
+		root = n;
 	}
-	
-	private static void showTree(EarleyParser.Node root,int level) //modificar para em vez de imprimir adicionar ao grafo
+	public void show()
+	{
+		showTree(root,0);
+	}
+	private void showTree(EarleyParser.Node root,int level) //modificar para em vez de imprimir adicionar ao grafo
 	{
 		System.out.print(level);
 		for(int i = 0; i <= level; i++)
@@ -35,5 +24,34 @@ public class Main {
 			showTree(sibling,level+1);
 		}
 	}
+}
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		Grammar g;
+		ArrayList<MyTree> trees = new ArrayList<MyTree>();
+		try {
+			g = new Grammar("./ficheiros_teste/grammar1.txt");
+			Lines lines=new Lines("./ficheiros_teste/sentences1.txt");
+			EarleyParser ep = new EarleyParser(lines.getLines().get(0), g);
+			boolean result = ep.run();
+			System.out.println("Result: " + result);
+			for(EarleyParser.Node node : ep.getTrees())
+					{
+						trees.add(new MyTree(node));
+					};
+					
+			for(MyTree tree : trees)
+			{
+				tree.show();
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR");
+		}
+	}
+	
+	
 	
 }
